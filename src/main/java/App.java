dec96d6c62cdc34bd891d9017ceb2b13db150255
zecollokaris <collo.kariss@gmail.com>
2018-10-13 19:19:15 +0300

    created database test class

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+// IMPORTS
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import  static spark.Spark.*;
+
+public class App{
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/DB.java
@@ -0,0 +1,5 @@
+import org.sql2o.*;
+
+public class DB {
+    public static Sql2o sql2o =
+}
