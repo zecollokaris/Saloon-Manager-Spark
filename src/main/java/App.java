b8ec1da6a47f327e8923821a6d243e3980bbdbda
zecollokaris <collo.kariss@gmail.com>
2018-10-17 00:09:57 +0300

    added route to redirect stylist add

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,15 +26,28 @@ public class App{
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-        post("addStylist",(resuest,response) ->{
-            Spring id=request.queryParams("id");
-            if (id.length()>8){
+        post("/addStylist",(request,response)->{
+            String id=request.queryParams("id");
+            if(id.length()>8){
                 id=id.substring(0,8);
             }
             Stylist stylist=new Stylist(
-                    
-            )
-        });
+                    Double.parseDouble(id),
+                    req.queryParams("first_name"),
+                    req.queryParams("second_name"),
+                    req.queryParams("last_name"),
+                    req.queryParams("phone_number"),
+                    req.queryParams("email")
+            );
+            if(stylist.save()){
+                model.put("message","Successfully Added Stylist");
+            }
+            else{
+                model.put("message","Id Number Exists");
+            }
+            res.redirect("/");
+            return new ModelAndView(model,"templates/layout.vtl");
+        },new VelocityTemplateEngine());
 
 
     }
