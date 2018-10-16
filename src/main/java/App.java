81a0d06abb3b45e2d0a27856289dd20051bbb4f9
zecollokaris <collo.kariss@gmail.com>
2018-10-17 00:25:04 +0300

    removed one hashmap from a get method to allow whole class to use one key value and pair

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,12 +9,12 @@ import  static spark.Spark.*;
 // APP Class
 public class App{
     static DB db=new DB();
+    static Map<String,Object> model=new HashMap<String,Object>();
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
         get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
             model.put("stylist",null);
             if(db.allData().size()>0){
                 model.put("stylist",db.allData());
@@ -26,6 +26,7 @@ public class App{
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
         post("/addStylist",(request,response)->{
             String id=request.queryParams("id");
             if(id.length()>8){
@@ -33,11 +34,11 @@ public class App{
             }
             Stylist stylist=new Stylist(
                     Double.parseDouble(id),
-                    req.queryParams("first_name"),
-                    req.queryParams("second_name"),
-                    req.queryParams("last_name"),
-                    req.queryParams("phone_number"),
-                    req.queryParams("email")
+                    request.queryParams("first_name"),
+                    request.queryParams("second_name"),
+                    request.queryParams("last_name"),
+                    request.queryParams("phone_number"),
+                    request.queryParams("email")
             );
             if(stylist.save()){
                 model.put("message","Successfully Added Stylist");
@@ -45,7 +46,7 @@ public class App{
             else{
                 model.put("message","Id Number Exists");
             }
-            res.redirect("/");
+            response.redirect("/");
             return new ModelAndView(model,"templates/layout.vtl");
         },new VelocityTemplateEngine());
 
