04e64042159fbebd11f4ad12ba95b4c2bee48765
zecollokaris <collo.kariss@gmail.com>
2018-10-17 14:27:47 +0300

    creating public class client that will access objects in private class

---
+++ src/main/java/Client.java
@@ -0,0 +1,14 @@
+import org.sql2o.Connection;
+public class Client {
+    private double id;
+    private String first_name;
+    private String second_name;
+    private String last_name;
+    private String stylist;
+    private String phone_number;
+    private String email;
+    private DB db=new DB();
+    public Client(double id,String first_name,String second_name, String last_name, String stylist, String phone_number, String email) {
+        
+    }
+}
