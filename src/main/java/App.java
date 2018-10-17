1f245072bb7083c16eee78f2ffe15d9d557a19ad
zecollokaris <collo.kariss@gmail.com>
2018-10-17 08:13:00 +0300

    trying to add stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,6 +14,24 @@ public class App{
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+//      Route to Post Stylists!
+        post("/newstylist",(request,response)->{
+            String id=request.queryParams("id");
+            Stylist stylist=new Stylist(
+            Double.parseDouble(id),
+            request.queryParams("first_name"),
+            request.queryParams("second_name"),
+            request.queryParams("last_name"),
+            request.queryParams("phone_number"),
+            request.queryParams("email")
+            );
+            stylist.save();
+            response.redirect("/");
+            return new ModelAndView(model,"templates/layout.vtl");
+        },new VelocityTemplateEngine());
+
+
+//      Route for home That fetches & displays stylists
         get("/", (request, response) -> {
             model.put("stylist",null);
             if(db.allData().size()>0){
@@ -25,30 +43,6 @@ public class App{
             model.put("template", "templates/index.vtl" );
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
-
-
-        post("/addStylist",(request,response)->{
-            String id=request.queryParams("id");
-            if(id.length()>8){
-                id=id.substring(0,8);
-            }
-            Stylist stylist=new Stylist(
-                    Double.parseDouble(id),
-                    request.queryParams("first_name"),
-                    request.queryParams("second_name"),
-                    request.queryParams("last_name"),
-                    request.queryParams("phone_number"),
-                    request.queryParams("email")
-            );
-            if(stylist.save()){
-                model.put("message","Successfully Added Stylist");
-            }
-            else{
-                model.put("message","Id Number Exists");
-            }
-            response.redirect("/");
-            return new ModelAndView(model,"templates/layout.vtl");
-        },new VelocityTemplateEngine());
 
 
     }
